f318b6c4c85c3b7533e068f42b72db7998b44085
slmredmon <shannonredmon@outlook.com>
2026-09-23 22:00:53 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase project configuration.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDmpSs1PRjosX6buyJrZKkO9fTYDG85Go4",
+  authDomain: "thriller-society.firebaseapp.com",
+  projectId: "thriller-society",
+  storageBucket: "thriller-society.appspot.com",
+  messagingSenderId: "823957700520",
+  appId: "1:823957700520:web:b1026ce148fc66639a279d"
+};
